fff53c25c2bbc8c09c552ed2c55e2c9ad2c278bd
Dinesh <dineshpal.dexbytes@gmail.com>
2021-05-05 11:28:02 +0530

    Main dart file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fullter_main_app/src/pages/home_new_page.dart';
+import 'package:fullter_main_app/src/widgets/alerts/confirmation_alert.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,32 @@ class MyApp extends StatelessWidget {
       title: "Flutter Component",
       home: Scaffold(
         //appBar: AppBar(title: Text("Flutter Component")),
-        body: HomeNewPage(),
+        body: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              ConfirmationAlert(
+                  context: context,
+                  noCallback: (alertContext) {},
+                  callBackYes: (alertContext) {});
+            },
+            child: Text("Confirmation"),
+          )
+        ],
       ),
     );
   }

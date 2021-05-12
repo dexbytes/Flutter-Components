19e41826c504d35ee301eb99fd2c9c2dff67c432
Dinesh <dineshpal.dexbytes@gmail.com>
2021-05-12 21:02:29 +0530

    Signup form screens

--- lib/src/pages/home_new_page.dart
+++ lib/src/pages/home_new_page.dart
@@ -4,29 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fullter_main_app/src/api_calling/api_constant.dart';
 import 'package:fullter_main_app/src/model/screen_list_model.dart';
-import 'package:fullter_main_app/src/pages/action_sheet_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/alert_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/badges_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/buttons_page.dart';
-
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/check_box_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/datepicker_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/divider_avatar_list_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/divider_icon_list_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/divider_list_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/divider_thumnail_list_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/floating_button_page.dart';
-
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/item_list_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/custom_switch_page.dart';
-import 'package:fullter_main_app/src/pages/razor_pay_page.dart';
 import 'package:fullter_main_app/src/pages/sign_up_form.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/tab_bar_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/search_view_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/select_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/toolbar_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/popover_page.dart';
-import 'file:///F:/FlutterAppWorkspace/Live_Flutter_Projects/modules_flutter/flutter_module_app/flutter_all_module_app/Flutter-Components/lib/src/pages/components/radio_page.dart';
 import 'package:fullter_main_app/src/widgets/DraggableFloatingActionButton.dart';
 import 'package:fullter_main_app/src/widgets/android_ios_check_box.dart';
 
@@ -45,9 +23,8 @@ class _HomeNewPageState extends State<HomeNewPage>
       mDraggableFloatingActionButtonController =
       DraggableFloatingActionButtonController();
   List<ScreenListModel> screenList = [
-    ScreenListModel(screen: SignUpPage(), title: "SignUpPage"),
-    ScreenListModel(screen: ComponentsPage(), title: "Components"),
-    ScreenListModel(screen: FormPage(), title: "Components1"),
+    ScreenListModel(screen: SignUpPage(), title: "SignUpPages"),
+    ScreenListModel(screen: ComponentsPage(), title: "Components")
   ];
 
   @override
